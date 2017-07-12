6e9bf1aadbf56b6ac5b1bc1341451fe6e6185297
System Administrator <root@localits-MacBook-Pro.local>
2017-07-12 12:05:49 +0530

    Successful login with encrypted credentials

--- SpringMVC/src/main/java/repositoryImpl/UserRepositoryImpl.java
+++ SpringMVC/src/main/java/repositoryImpl/UserRepositoryImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import repository.UserRepository;
 import vo.BusinessException;
+import vo.Status;
 import vo.User;
 
 @Repository("userRepository")
@@ -17,8 +18,17 @@ public class UserRepositoryImpl implements UserRepository {
 	
 	public String SetDetails(User user) throws BusinessException{
 		String methodname="SetDetails";
-		System.out.println(methodname+" stats");
-		return ""+jdbcTemplate.update((new StringBuilder("insert into SPRINGPROJECT.UserDetails(UserName,UserPassword) Values (?,?)")).toString(),new Object[]{user.getName(),user.getPassword()});
+		System.out.println(methodname+" starts");
+		try{
+		if(jdbcTemplate.update((new StringBuilder("insert into SPRINGPROJECT.UserDetails(UserName,UserPassword) Values (?,?)")).toString(),new Object[]{user.getName(),user.getPassword()})==1)
+			return "Successful Login";
+		else{
+			return "Not able to Login";
+		}
+		}
+		catch(Exception e){
+			throw new BusinessException(new Status("Something went wrong",500), e);	
+		}
 	}
 
 	
