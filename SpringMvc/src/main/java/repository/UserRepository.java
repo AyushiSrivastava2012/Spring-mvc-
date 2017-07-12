244f6dbbfe36509d27f02e2846f49f495bea4b8b
System Administrator <root@localits-MacBook-Pro.local>
2017-07-12 11:32:32 +0530

    Remove duplicated folder

--- SpringMvc/src/main/java/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import vo.BusinessException;
-import vo.User;
-
-public interface UserRepository {
-
-	String SetDetails(User user) throws BusinessException;
-}
--- SpringMvc/src/main/java/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import vo.BusinessException;
-import vo.User;
-
-public interface UserService {
-
-	String SetDetails(User user) throws BusinessException;
-}
